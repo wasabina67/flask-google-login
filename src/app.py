a2b60429a20e9fc3f38637a27c386c298da7b32d
wasabina67 <wasabina67@gmail.com>
2024-12-04 12:29:36 +0000

    Update app.py

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from flask import Flask, redirect, render_template, session
+from oauthlib.oauth2 import WebApplicationClient  # type: ignore
 
 from utils import get_openid_configuration
 
@@ -10,9 +11,17 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
 
+client_id = os.getenv("GOOGLE_CLIENT_ID")
+client_secret = os.getenv("GOOGLE_CLIENT_SECRET")
 openid_configuration = get_openid_configuration(
     os.getenv("GOOGLE_OPENID_CONFIGURATION")
 )
+authorization_endpoint = openid_configuration["authorization_endpoint"]
+token_endpoint = openid_configuration["token_endpoint"]
+userinfo_endpoint = openid_configuration["userinfo_endpoint"]
+redirect_uri = "https://localhost:3443/callback"
+
+client = WebApplicationClient(client_id)
 
 
 @app.route("/")
